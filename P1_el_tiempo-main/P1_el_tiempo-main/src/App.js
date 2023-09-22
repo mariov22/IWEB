1f807eb84112cdfc14a5a4a7fccb255caaae1f10
mariov22 <albertosjimenez@gmail.com>
2023-09-22 13:02:12 +0200

    fetch

--- P1_el_tiempo-main/P1_el_tiempo-main/src/App.js
+++ P1_el_tiempo-main/P1_el_tiempo-main/src/App.js
@@ -2,41 +2,42 @@ import './App.css';
 import Header from './Header';
 import Resultados from './Resultados';
 import CONFIG from './config/config';
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { mock1 } from './constants/mock';
 
 function App() {
 
   const [lat, setLat] = useState(CONFIG.default_lat);
   const [lon, setLon] = useState(CONFIG.default_lon);
+  const [datos, setDatos] = useState();
 
-  useEffect(() => {
-    async function fetchData(){
-      try{
-        if(CONFIG.use_server){
-          let resultado = await fetch(CONFIG.server_url);
-          let resultadoJSON = await resultado.json();
-          //resultadoJSON.cosas que necesites de la api para pintar la página
-        }else{
-          let resultado = await fetch({mock1});
-        }
-      } catch(e) {
-        console.log("", e);
+  const fetchData = async (param) => {
+    if (CONFIG.use_server){
+      try {
+
+        let apiDatos = await fetch(CONFIG.server_url);
+        console.log(apiDatos);
+        setDatos(apiDatos);
+
+      } catch (error){
+        console.log("Error");
       }
+    } else {
+        setDatos(mock1);
     }
-    fetchData();
-  })
+  }
+  
 
   return (
     <div className="App">
       <Header />
       <div>
-            <h2 id = "titulo"> El tiempo </h2>
-            <input type ="number" id = "latitud" value = {lat} onChange={e => setLat(e.target.value)}></input>
-            <input type ="number" id = "longitud" value = {lon} onChange={e => setLon(e.target.value)}></input>
-            <button id = "buscar"> Buscar </button>
-        </div>
-        <Resultados numitems = {CONFIG.num_items} items = {mock1}/>
+          <h2 id = "titulo"> El tiempo </h2>
+          <input type ="number" id = "latitud" value = {lat} onChange={e => setLat(e.target.value)}></input>
+          <input type ="number" id = "longitud" value = {lon} onChange={e => setLon(e.target.value)}></input>
+          <button id = "buscar" onClick={()=>fetchData()}> Buscar </button>
+      </div>
+      {datos && <Resultados numitems = {CONFIG.num_items} items = {datos}/>}
     </div>
   );
 }
